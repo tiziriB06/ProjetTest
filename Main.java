d97b34875b4aedf68eb9549fb51c8364d2e120f8
Tiziri6005 <t_bouaoud@estin.dz>
2025-10-02 22:49:38 +0100

    Ajout du code Java initial

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Gaulois asterix = new Gaulois("Asterix", 10);
+        Romain minus = new Romain("Minus", 5);
+        
+
+        asterix.parler("Bonjour !");
+        minus.parler(" Je suis un romain.");
+    }
+}

ec5d74b1a6fceebff338ef986d24f6e57fc48e5b
Tiziri6005 <t_bouaoud@estin.dz>
2025-10-02 23:03:33 +0100

    Mise à jour de la classe Gaulois

--- Gaulois.java
+++ Gaulois.java
@@ -1,1 +1,43 @@
-"Bonjour, je suis un nouveau fichier !" 
+public class Gaulois { 
+ private String nom; 
+ private int force; 
+ 
+ public Gaulois(String nom, int force) { 
+  this.nom = nom; 
+  this.force = force; 
+ } 
+ 
+ public String getNom() { 
+  return nom; 
+ } 
+ 
+ public void parler(String texte) { 
+  System.out.println(prendreParole() + "\"" + texte + "\""); 
+ } 
+ 
+ private String prendreParole() { 
+  return "Le gaulois " + nom + " : "; 
+ } 
+} 
+ 
+ class Romain { 
+ private String nom; 
+ private int force; 
+  
+ public Romain(String nom, int force) { 
+  this.nom = nom; 
+  this.force = force; 
+ } 
+  
+ public String getNom() { 
+  return nom; 
+ } 
+  
+ public void parler(String texte) { 
+  System.out.println(prendreParole() + "\"" + texte + "\""); 
+ } 
+ 
+ private String prendreParole() { 
+  return "Le romain " + nom + " : "; 
+ } 
+} 
